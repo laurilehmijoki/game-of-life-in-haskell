0ee272353318dd74157594dc0cc9b9453e7596e7
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2013-03-30 12:34:11 +0200

    Place rules into its own module

--- Game.hs
+++ Game.hs
@@ -1,42 +1,12 @@
 module Game(nextGeneration, createWorld) where
 
-import Debug.Trace(trace)
 import System.Random(randomR, StdGen)
 import qualified Model as M
+import qualified Rules as R
 
 nextGeneration cellMatrix = map playGameOfLife cellMatrix
   where
-    playGameOfLife cellArray = map maybeReproduce cellArray
-    maybeReproduce aliveCell@(M.AliveCell(_)) = aliveCell
-    maybeReproduce emptyCell@(M.EmptyCell(point)) =
-      if (countNeighbours emptyCell isAlive cellMatrix) == 3
-        then M.AliveCell(point)
-        else emptyCell
-
-countNeighbours cell cellState cellMatrix =
-  length $ filter cellState $ neighbours (point cell) cellMatrix
-
-neighbours (M.Point(y, x)) cellMatrix =
-  filter (isNeighbour x y) $ flatten cellMatrix
-  where
-    flatten = foldl (++) []
-
-isNeighbour x y cell =
-  deltaX <= 1 && deltaY <= 1
-  where
-    deltaX = maxX - minX
-    deltaY = maxY - minY
-    maxX = max x x2
-    maxY = max y y2
-    minX = min x x2
-    minY = min y y2
-    (M.Point(y2, x2)) = point cell
-
-point (M.EmptyCell(pt)) = pt
-point (M.AliveCell(pt)) = pt
-
-isAlive (M.AliveCell(_))      = True
-isAlive (M.EmptyCell(_)) = False
+    playGameOfLife cellArray = map (R.reproductionRule cellMatrix) cellArray
 
 createWorld rectangle@(M.Rectangle (width, height)) randomGen =
   M.World(rectangle, map createCellMatrix xs)
---
+++ Rules.hs
@@ -0,0 +1,35 @@
+module Rules(reproductionRule) where
+
+import Debug.Trace(trace)
+import qualified Model as M
+
+reproductionRule cellMatrix aliveCell@(M.AliveCell(_)) = aliveCell
+reproductionRule cellMatrix emptyCell@(M.EmptyCell(point)) =
+  if (countNeighbours emptyCell isAlive cellMatrix) == 3
+    then M.AliveCell(point)
+    else emptyCell
+
+countNeighbours cell cellState cellMatrix =
+  length $ filter cellState $ neighbours (point cell) cellMatrix
+
+neighbours (M.Point(y, x)) cellMatrix =
+  filter (isNeighbour x y) $ flatten cellMatrix
+  where
+    flatten = foldl (++) []
+
+isNeighbour x y cell =
+  deltaX <= 1 && deltaY <= 1
+  where
+    deltaX = maxX - minX
+    deltaY = maxY - minY
+    maxX = max x x2
+    maxY = max y y2
+    minX = min x x2
+    minY = min y y2
+    (M.Point(y2, x2)) = point cell
+
+point (M.EmptyCell(pt)) = pt
+point (M.AliveCell(pt)) = pt
+
+isAlive (M.AliveCell(_))      = True
+isAlive (M.EmptyCell(_)) = False
